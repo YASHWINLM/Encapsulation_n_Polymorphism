2b9fd23b9228339a5da5de98c64738e040423db1
League <league@WTS9.local>
2018-08-10 18:28:34 -0700

    Finish hospital

--- src/_04_hospital/Doctor.java
+++ src/_04_hospital/Doctor.java
@@ -1,9 +1,12 @@
 package _04_hospital;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Doctor {
 
+	ArrayList<Patient> AssignedPatients= new ArrayList<Patient>();
+	
 	public Object performsSurgery() {
 		// TODO Auto-generated method stub
 		return null;
@@ -16,7 +19,7 @@ public class Doctor {
 
 	public void assignPatient(Patient patient) {
 		// TODO Auto-generated method stub
-		
+		AssignedPatients.add(patient);
 	}
 
 	public List<Doctor> getPatients() {
--- src/_04_hospital/Hospital.java
+++ src/_04_hospital/Hospital.java
@@ -1,36 +1,58 @@
 package _04_hospital;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hospital {
 
+	ArrayList<Doctor> doctors= new ArrayList<Doctor>();
+	ArrayList<Patient> patients= new ArrayList<Patient>();
 	public void addDoctor(GeneralPractitioner generalPractitioner) {
 		// TODO Auto-generated method stub
-		
+		doctors.add(generalPractitioner);
 	}
 
 	public List<Doctor> getDoctors() {
 		// TODO Auto-generated method stub
-		return null;
+		List<Doctor> Doctor= new ArrayList<Doctor>();
+		for (int i = 0; i < doctors.size(); i++) {
+			Doctor.add(doctors.get(i));
+		}
+		
+		return Doctor;
 	}
 
 	public void addDoctor(Surgeon surgeon) {
 		// TODO Auto-generated method stub
+		doctors.add(surgeon);
 		
 	}
 
 	public void addPatient(Patient patient) {
 		// TODO Auto-generated method stub
+		patients.add(patient);
 		
 	}
 
 	public List<Doctor> getPatients() {
 		// TODO Auto-generated method stub
+	
+		
 		return null;
 	}
 
 	public void assignPatientsToDoctors() {
 		// TODO Auto-generated method stub
+		
+		for (int i = 0; i < doctors.size(); i++) {
+			for (int j = 0; j < patients.size(); j++) {
+				for (int j2 = 0; j2 < 3; j2++) {
+					doctors.get(i).assignPatient(patients.get(j));
+				}
+			}
+			
+			
+		}		
 		
 	}
 
